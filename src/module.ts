c69dece69cb07db6b0744753603713de7b4c7f5a
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-06-11 00:14:23 +0200

    fix: ensure all exported functions are present in every environment

--- src/module.ts
+++ src/module.ts
@@ -1,24 +1,19 @@
 import { load } from 'array-buffer-cache-broker';
+import { createLoadOrReturnBroker } from './factories/load-or-return-broker';
 import { worker } from './worker/worker';
 
-const blob: Blob = new Blob([worker], { type: 'application/javascript; charset=utf-8' });
+const loadOrReturnBroker = createLoadOrReturnBroker(load, worker);
 
-const url: string = URL.createObjectURL(blob);
+export const clone: ReturnType<typeof load>['clone'] = (arrayBufferId) => loadOrReturnBroker().clone(arrayBufferId);
 
-const arrayBufferCache = load(url);
+export const connect: ReturnType<typeof load>['connect'] = () => loadOrReturnBroker().connect();
 
-export const clone = arrayBufferCache.clone;
+export const disconnect: ReturnType<typeof load>['disconnect'] = (port) => loadOrReturnBroker().disconnect(port);
 
-export const connect = arrayBufferCache.connect;
+export const isSupported: ReturnType<typeof load>['isSupported'] = () => loadOrReturnBroker().isSupported();
 
-export const disconnect = arrayBufferCache.disconnect;
+export const purge: ReturnType<typeof load>['purge'] = (arrayBufferId) => loadOrReturnBroker().purge(arrayBufferId);
 
-export const isSupported = arrayBufferCache.isSupported;
+export const slice: ReturnType<typeof load>['slice'] = (...args) => loadOrReturnBroker().slice(...args);
 
-export const purge = arrayBufferCache.purge;
-
-export const slice = arrayBufferCache.slice;
-
-export const store = arrayBufferCache.store;
-
-URL.revokeObjectURL(url);
+export const store: ReturnType<typeof load>['store'] = (arrayBuffer) => loadOrReturnBroker().store(arrayBuffer);
